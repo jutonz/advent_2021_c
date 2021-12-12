993dd69a73cb0e19865f808a51f3f3ea6fe61cee
Justin Toniazzo <jutonz42@gmail.com>
2021-12-12 08:29:24 -0500

    Use cmake

---
+++ readme.md
@@ -0,0 +1,19 @@
+## Initial setup
+
+```bash
+cmake -S . -B build/
+cmake --build build
+```
+
+## Running
+
+```bash
+./build/day1-1 /path/to/input.txt
+```
+
+## Recompiling
+
+```bash
+cd build
+make
+```
--- src/day1/part1/main.c
+++ src/day1/part1/main.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-FILE* open_file() {
-  FILE *fp = fopen("input.txt", "r");
+FILE* open_file(const char *filename) {
+  FILE *fp = fopen(filename, "r");
 
   if (fp == NULL) {
     perror("Could not open file\n");
@@ -13,15 +13,22 @@ FILE* open_file() {
   return fp;
 }
 
-int main() {
-  FILE *fp = open_file();
-  int bufferLength = 255;
-  char buffer[bufferLength];
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    char error[256];
+    snprintf(error, 256, "Usage: %s /path/to/input.txt", argv[0]);
+    printf("%s\n", error);
+    exit(1);
+  }
+
+  FILE *fp = open_file(argv[1]);
+  static const short BUFFER_LENGTH = 255;
+  char buffer[BUFFER_LENGTH];
 
   bool initial = true;
   int previous, current;
   int increases = 0;
-  while(fgets(buffer, bufferLength, fp)) {
+  while(fgets(buffer, BUFFER_LENGTH, fp)) {
     sscanf(buffer, "%d", &current);
 
     if (initial) {
