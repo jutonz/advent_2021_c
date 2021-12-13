29ca697767e3e358cc70700c020fa5c209924e8b
Justin Toniazzo <jutonz42@gmail.com>
2021-12-13 09:20:13 -0500

    Day 1, part 2

---
+++ src/day1/part2/helpers.h
@@ -0,0 +1,8 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+void initialize_window(int *window, int size);
+void add_to_window(int *window, int size, int value);
+int sum_window(int *window, int size);
+
+#endif
